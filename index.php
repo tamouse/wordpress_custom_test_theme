9623cbfc5950caf74f29ce7c7472bc676af5bfbc
Tamara Temple <tamouse@gmail.com>
2016-10-28 23:54:41 -0500

    working start

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Test Theme
+Plugin URI: https://github.com/tamouse/wordpress_custom_test_theme
+Description: A wordpress custom test theme
+Version: 0.1.0
+Author: Tamara Temple
+Author URI: https://github.com/tamouse
+Text Domain: test-theme
+Domain Path: /languages
+*/
+
